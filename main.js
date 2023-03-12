bdcbdcc8eb98b2dc9194797fc2cf9c0d7ecacf52
wponc <poncyw@gmail.com>
2023-03-11 23:46:09 -0500

    coil favicon added, removed unused assets

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 import './style.css'
 import * as THREE from 'three';
 
+const sections = document.querySelectorAll('.section')
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("show", entry.isIntersecting)
+    if (entry.isIntersecting) observer.unobserve(entry.target)
+  })
+  }, {
+  threshold: .25,
+  }
+)
+
+sections.forEach(section => {
+  observer.observe(section)
+})
 
 
-const white = new THREE.Color();
 
 // Clock for stepping through animation loop 
 const clock = new THREE.Clock();
@@ -28,8 +41,6 @@ renderer.toneMapping = THREE.ACESFilmicToneMapping;
 
 // Manager for loading animation
 const manager = new THREE.LoadingManager();
-
-
 const textureloader = new THREE.TextureLoader(manager);
 
 
@@ -38,19 +49,6 @@ const gold = textureloader.load('assets/matcaps/gold.jpg');
 const multi = textureloader.load('assets/matcaps/multi.jpg');
 const silv = textureloader.load('assets/matcaps/silv.jpg');
 const obsidian = textureloader.load('assets/matcaps/obsidian.jpg');
-
-
-// Vertex shader uniforms
-const uniforms = {
-  u_time: {type: 'f', value: 0.0},
-  time2: {type: 'f', value: 0.0}
-}
-
-// Size constants
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
-}
 
 // Scene & camera creation
 const cameraGroup = new THREE.Group()
@@ -62,24 +60,22 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 scene.add(cameraGroup)
+
 // Rendering
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.set(0,0,0);
 cameraGroup.add(camera)
-//scene.background = new THREE.Color(0x3b4744)
-
-
 
 // Rotating torus knot visible on page load
 const torusgeom = new THREE.TorusKnotGeometry(2,.1,300,20,4,9)
 const torusmat = new THREE.MeshMatcapMaterial();
-torusmat.matcap = obsidian;
+torusmat.matcap = gold;
 const torusmesh = new THREE.Mesh(torusgeom, torusmat);
 scene.add(torusmesh);
-torusmesh.position.set(2.5,0,-1);
+torusmesh.position.set(2,0,-.5);
 torusmesh.rotateY(300);
 torusmesh.rotateX(37);
+
 
 // Event listener for window resize
 window.addEventListener( 'resize', onWindowResize, false );
@@ -89,6 +85,7 @@ function onWindowResize(){
   renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
+// Brings viewer to section visible before page was refreshed 
 document.addEventListener("DOMContentLoaded", function(event) { 
   var scrollpos = localStorage.getItem('scrollpos');
   if (scrollpos) window.scrollTo(0, scrollpos);
@@ -97,8 +94,13 @@ window.onbeforeunload = function(e) {
   localStorage.setItem('scrollpos', window.scrollY);
 };
 
+// Size constants for use in parallax
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
 
-
+// Cursor tracking for parallax effect
 const cursor = {}
 cursor.x = 0
 cursor.y = 0
@@ -106,24 +108,26 @@ window.addEventListener('mousemove', (event) =>
 {
     cursor.x = event.clientX / sizes.width - 0.5
     cursor.y = event.clientY / sizes.height - 0.5
-
-    console.log(cursor)
 })
+
 
 // Animation loop to rotate torus
 let previousTime = 0
+
 function animate(){
   requestAnimationFrame(animate);
+
   const elapsedTime = clock.getElapsedTime()
   const deltaTime = elapsedTime - previousTime
   previousTime = elapsedTime
-  torusgeom.rotateZ(.0005);
-  renderer.render(scene, camera);
+
+  torusgeom.rotateZ(.0003);
   const parallaxX = cursor.x * .1;
   const parallaxY = -cursor.y * .1;
-
   cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
   cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+
+  renderer.render(scene, camera);
 }
 
 animate();
